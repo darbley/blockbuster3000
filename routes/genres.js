7f8b4367c25352e861e64d0c0a4071541750bb2f
Darbley <stapleton.b@gmail.com>
2021-05-06 17:00:49 -0400

    add some basic crud for genres

--- routes/genres.js
+++ routes/genres.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
 
 const genres = [
     { id: 1, name: 'Action' },  
@@ -10,9 +11,9 @@ const genres = [
   
 // GET REQUESTS
 router.get('/', (req, res) => {
-    //res.send("Hello W")
-    res.render('index', { title: 'Genres page', message: 'Pug message text'})
+    res.send(genres)
 })
+
 
 router.get('/:id', (req, res) => {
     let genre = genres.find((genre) => {
@@ -20,10 +21,83 @@ router.get('/:id', (req, res) => {
         return genre.id === paramId
     })
     if(!genre){
-        return res.status(404).send('the course id was not found')
+        return res.status(404).send('the film genre id was not found')
     }
     res.send(genre);
 })
+
+// POST REQUESTS
+router.post('/', (req, res) => {
+ 
+    const { error } = validateGenre(req.body);
+    if(error){
+        console.log(error);
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+    genres.push(genre);
+    res.send(genre);
+})
+
+const validateGenre = (obj) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    
+    return schema.validate(obj);
+}
+
+// UPDATE - PUT REQUESTS
+router.put('/:id', (req, res) => {
+    // Look up the course
+    let genre = genres.find((genre) => {
+        let paramId = parseInt(req.params.id);
+        return genre.id === paramId
+    })
+    // If not existing, return 404 not found
+    if(!genre){
+        return res.status(404).send('the film genre id was not found')
+    }
+
+    // Validate 
+    // IF invalid, return 400,- Bad request
+    
+    const { error } = validateGenre(req.body);
+    if(error){
+        console.log(error);
+        return res.status(400).send(error.details[0].message);
+    }
+   
+    // Update the course
+    // Return the updated course
+    genre.name = req.body.name;
+    res.send(genre);
+
+})
+
+// DELETE
+router.delete('/:id', (req, res) => {
+    // Look up the course
+    let genre = genres.find((genre) => {
+        let paramId = parseInt(req.params.id);
+        return genre.id === paramId
+    })
+    // If not existing, return 404 not found
+    if(!genre){
+        return res.status(404).send('the film genre id was not found ');
+    }
+    console.log('film genre found',genre)
+    // DELETE
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+    console.log('genre found: ',index)
+    res.send(`deleted ${genre}`)
+})
+
 
 
 module.exports = router;
